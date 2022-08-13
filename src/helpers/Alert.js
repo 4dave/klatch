307e24b49250e99d33f2c7b2603d4cdcfe0b15eb
David Fredkove <dave.fredkove@gmail.com>
2022-08-12 23:51:18 -0500

    upd bgcol, testing fa i alerts, flex-col and width md and above

--- src/helpers/Alert.js
+++ src/helpers/Alert.js
@@ -5,24 +5,28 @@ function Alert({ type, message }) {
     case "success":
       return (
         <div className="alert alert-success" role="alert">
+          <i class="fa fa-check"></i>
           <strong>{message}</strong>
         </div>
       )
     case "error":
       return (
         <div className="alert alert-error" role="alert">
+          <i class="fa fa-times-circle"></i>
           <strong>{message}</strong>
         </div>
       )
     case "warning":
       return (
         <div className="alert alert-warning" role="alert">
+          <i class="fa fa-warning"></i>
           <strong>{message}</strong>
         </div>
       )
     case "info":
       return (
         <div className="alert alert-info" role="alert">
+          <i class="fa fa-info-circle"></i>
           <strong>{message}</strong>
         </div>
       )
